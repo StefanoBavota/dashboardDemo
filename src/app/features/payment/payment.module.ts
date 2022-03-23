d513504cb70a7910e295f7df5792bbecb1640b5d
Giovanni <giovanni.dellapelle@aesys.tech>
2022-03-23 14:50:37 +0100

    Added routing to modules

--- src/app/features/payment/payment.module.ts
+++ src/app/features/payment/payment.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { PaymentRoutingModule } from './payment-routing.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    PaymentRoutingModule
   ]
 })
 export class PaymentModule { }
