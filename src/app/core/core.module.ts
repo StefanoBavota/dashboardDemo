ed9a2a7e1ecd228f4b2564864327558c0110039d
StefanoBavota <stefano.bavota@hotmail.com>
2022-03-24 14:34:57 +0100

    feature(navbar component): Add navbar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [SidebarComponent],
+  declarations: [SidebarComponent, NavbarComponent],
   imports: [CommonModule, HttpClientModule],
-  exports: [SidebarComponent],
+  exports: [SidebarComponent, NavbarComponent],
 })
 export class CoreModule {}
