1ec0c0e5fb62d9faa92f5ecd83c7b2ad8c616b6b
Giovanni <giovanni.dellapelle@aesys.tech>
2022-03-24 14:30:46 +0100

    Pagination component working

--- src/app/features/demo/demo.module.ts
+++ src/app/features/demo/demo.module.ts
@@ -1,11 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CoreModule } from 'src/app/core/core.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { DemoRoutingModule } from './demo-routing.module';
 import { DemoComponent } from './demo.component';
 
 @NgModule({
-  declarations: [DemoComponent],
-  imports: [CommonModule, DemoRoutingModule, CoreModule],
+  declarations: [
+    DemoComponent
+  ],
+  imports: [
+    CommonModule,
+    DemoRoutingModule,
+    CoreModule,
+    SharedModule
+  ],
 })
 export class DemoModule {}
