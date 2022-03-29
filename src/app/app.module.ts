3a3dd60e9af7dbcd0ec0cbd50e4f6c65bc9f2f4f
Giovanni <giovanni.dellapelle@aesys.tech>
2022-03-29 15:52:42 +0200

    client-edit minor fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, FormsModule, ReactiveFormsModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, FormsModule, ReactiveFormsModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
