c4262d1827899beb280b8ac74f8cce3e7c0fe272
StefanoBavota <stefano.bavota@hotmail.com>
2022-03-31 15:36:12 +0200

    Revert "Merge branch 'develop' of https://github.com/StefanoBavota/dashboardDemo into develop"
    This reverts commit f48966d746db236dd995cfc26c10be241f95655c, reversing
    changes made to a75d0ebb28aa9f76901489c44d596907b6d7724c.

--- src/app/features/demo/demo.component.ts
+++ src/app/features/demo/demo.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { from } from 'rxjs';
-import { ToastService } from 'src/app/core/services/toast.service';
+import { from, Observable } from 'rxjs';
 import { ChangePasswordComponent } from 'src/app/shared/components/change-password/change-password.component';
 import { ConfirmModalComponent } from 'src/app/shared/components/confirm-modal/confirm-modal.component';
-import { DeleteModalComponent } from 'src/app/shared/components/delete-modal/delete-modal.component';
 
 @Component({
   selector: 'app-demo',
@@ -17,10 +15,7 @@ export class DemoComponent implements OnInit {
     { name: 'option2', value: 2 },
     { name: 'option3', value: 3 },
   ];
-  constructor(
-    private modalService: NgbModal,
-    private toastService: ToastService
-    ) {}
+  constructor(private modalService: NgbModal) {}
 
   ngOnInit(): void {}
 
@@ -34,20 +29,5 @@ export class DemoComponent implements OnInit {
 
   openModalPassword() {
     this.modalService.open(ChangePasswordComponent);
-  }
-
-  createToast() {
-    this.toastService.show('prova header', 'prova body');
-  }
-
-  showDeleteModal() {
-    const modalRef = this.modalService.open(DeleteModalComponent);
-    modalRef.componentInstance.item = `item di prova`;
-
-    modalRef.result.then(modalRes => {
-      if(modalRes) {
-        this.toastService.show('Hai premuto conferma', `confermone`)
-      }
-    })
   }
 }
