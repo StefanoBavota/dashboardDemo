32ae0d5656553bb93a35603667b371da2f2faff8
Daniele-DF <daniele.difonte@aeys.tech>
2022-03-31 14:52:20 +0200

    remove <td>

--- src/app/features/admin-user/components/user-edit-page/user-edit-page.component.ts
+++ src/app/features/admin-user/components/user-edit-page/user-edit-page.component.ts
@@ -12,4 +12,57 @@ export class UserEditPageComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  populateFormControls() {
+    if(this.user && this.mode !== 'NEW') {
+      this.editFormUser.patchValue(this.user);
+      this.editFormUser.patchValue({
+        born: getDateInputFormat(this.client.born)
+      })
+    }
+    if(this.mode === 'DETAIL') {
+      this.editFormUser.disable();
+    }
+  }
+
+  enableModifies() {
+    this.changeMode('EDIT');
+    this.editFormUser.enable();
+  }
+
+  saveModifies() {
+    console.log('formGroup', this.editFormUser.value);
+    if(this.editFormUser.valid) {
+      if(this.mode === 'EDIT' && this.client) {
+        const newClient: Client = {
+          id: this.client?.id,
+          ...this.editFormUser.value
+        };
+        this.data.modifyClient(newClient).subscribe(res => {
+          console.log(res);
+          this.router.navigateByUrl('client')
+        });
+      }
+      else {
+        this.data.insertClient({
+          id: '',
+          ...this.editFormUser.value
+        }).subscribe(res => {
+          this.router.navigateByUrl('client');
+        });
+      }
+    }
+  }
+
+  changeMode(newMode: string) {
+    this.mode = newMode;
+    this.modeBS.next(newMode);
+  }
+  checkField(input: NgModel) {
+    return { 'is-invalid': input.invalid, 'is-valid': input.valid}
+  }
+
+  submitHandler(formData: any) {
+    console.log(formData)
+
+  }
 }
