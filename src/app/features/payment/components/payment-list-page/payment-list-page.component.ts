bc94d591574306e306a8661e8d8697c232f6a3e2
StefanoBavota <stefano.bavota@hotmail.com>
2022-04-04 11:44:14 +0200

    feature(payment-list): add view list without filter component

--- src/app/features/payment/components/payment-list-page/payment-list-page.component.ts
+++ src/app/features/payment/components/payment-list-page/payment-list-page.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { Page, Payment } from 'src/app/core/models';
+import { DataService } from 'src/app/core/services/data.service';
 
 @Component({
   selector: 'app-payment-list-page',
   templateUrl: './payment-list-page.component.html',
-  styleUrls: ['./payment-list-page.component.scss']
+  styleUrls: ['./payment-list-page.component.scss'],
 })
 export class PaymentListPageComponent implements OnInit {
+  payments: Payment[] = [];
 
-  constructor() { }
+  skip: number = 0;
+  take: number = 10;
+  totalPages: number = 1;
+  actualPage: number = 1;
+
+  constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
+    this.getPayments();
   }
 
+  onClickEdit(payment: Payment) {}
+
+  onClickDelete(payment: Payment) {}
+
+  getPayments() {
+    this.dataService.getPayments({}).subscribe((res: Page) => {
+      this.payments = res.data;
+      this.totalPages = Math.ceil(res.total / this.take);
+      console.log('Payments', this.payments, 'totalPages', this.totalPages);
+    });
+  }
+
+  onPageClick(page: number) {
+    this.skip = (page - 1) * this.take;
+    this.actualPage = page;
+    this.getPayments();
+  }
 }
--- src/app/features/payment/payment.module.ts
+++ src/app/features/payment/payment.module.ts
@@ -4,16 +4,20 @@ import { CommonModule } from '@angular/common';
 import { PaymentRoutingModule } from './payment-routing.module';
 import { PaymentListPageComponent } from './components/payment-list-page/payment-list-page.component';
 import { PaymentEditPageComponent } from './components/payment-edit-page/payment-edit-page.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     PaymentListPageComponent,
-    PaymentEditPageComponent
+    PaymentEditPageComponent,
   ],
   imports: [
     CommonModule,
-    PaymentRoutingModule
+    PaymentRoutingModule,
+    SharedModule,
+    ReactiveFormsModule,
   ]
 })
 export class PaymentModule { }
